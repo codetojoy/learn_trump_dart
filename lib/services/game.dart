9c7c2d6142733c3a6a9de4000551f2c5d81f932e
Michael Easter <codetojoy@gmail.com>
2021-06-27 19:48:24 -0300

    fixed console bugs

--- lib/services/game.dart
+++ lib/services/game.dart
@@ -7,12 +7,39 @@ import '../models/deck.dart';
 import './ranker.dart';
 import '../utils/constants.dart';
 
+class RoundInfo {
+  var firstGuess = true;
+  var roundNum = 1;
+  var numCorrect = 0;
+
+  RoundInfo.init() {}
+
+  RoundInfo(this.firstGuess, this.roundNum, this.numCorrect);
+
+  bool isDone(int numRounds) {
+    return roundNum >= numRounds;
+  }
+
+  RoundInfo nextRound() {
+    return RoundInfo(true, roundNum + 1, numCorrect);
+  }
+
+  RoundInfo correctGuess() {
+    final newNumCorrect = (firstGuess) ? numCorrect + 1 : numCorrect;
+    return RoundInfo(firstGuess, roundNum, newNumCorrect);
+  }
+
+  RoundInfo wrongGuess() {
+    return RoundInfo(false, roundNum, numCorrect);
+  }
+
+  @override
+  String toString() {
+    return '$numCorrect / $roundNum';
+  }
+}
+
 class Game {
-  static const _ILLEGAL_SELECTION = -1;
-
-  var _roundNum = 1;
-  var _numCorrect = 0;
-
   List<Card> _getCards(Suit trumpSuit) {
     var cards;
     final numCards = Config.instance.numCards;
@@ -26,17 +53,31 @@ class Game {
     return cards;
   }
 
-  int _getSelection(String input, int numCards) {
-    var result = _ILLEGAL_SELECTION;
-    final choiceIndex = int.parse(input);
-    final ok = choiceIndex >= 1 && choiceIndex <= numCards;
-    if (ok) {
-      result = choiceIndex;
+  Card _getSelection(List<Card> cards) {
+    var result = C.UNKNOWN;
+    var done = false;
+
+    while (!done) {
+      stdout.writeln('enter your choice (q to quit): ');
+      final input = stdin.readLineSync();
+      if (input! == 'q') {
+        stdout.writeln('OK quitting...');
+        exit(0);
+      }
+      final choiceIndex = int.parse(input);
+      final ok = choiceIndex >= 1 && choiceIndex <= cards.length;
+      if (ok) {
+        result = cards[choiceIndex - 1];
+        done = true;
+      } else {
+        stdout.writeln('illegal choice');
+      }
     }
     return result;
   }
 
-  void playRound() {
+  RoundInfo playRound(RoundInfo inRoundInfo) {
+    var roundInfo = inRoundInfo;
     final trumpSuit = Suits().getRandom();
     final leadingSuit = Suits().getRandom();
     final cards = _getCards(trumpSuit);
@@ -49,48 +90,31 @@ class Game {
     stdout.writeln('cards: $cards');
     stdout.writeln('sorted cards: $sortedCards');
 
-    var done = false;
-    var selection = C.UNKNOWN;
-    var firstGuess = true;
-    while (!done) {
-      stdout.writeln('enter your choice (q to quit): ');
-      final input = stdin.readLineSync();
-      if (input! == 'q') {
-        stdout.writeln('OK quitting...');
-        exit(0);
-      }
-      final selectedIndex = _getSelection(input, numCards);
-      final ok = selectedIndex != _ILLEGAL_SELECTION;
-      if (ok) {
-        selection = cards[selectedIndex - 1];
-        done = true;
-      } else {
-        print('illegal choice');
-      }
-      if (selection == sortedCards[numCards - 1]) {
-        stdout.writeln('correct!');
-        if (firstGuess) {
-          _numCorrect++;
-        }
-        stdout.writeln('$_numCorrect / $_roundNum');
-      } else {
-        stdout.writeln('try again');
-      }
+    final selection = _getSelection(cards);
+
+    if (selection == sortedCards[numCards - 1]) {
+      stdout.writeln('correct!');
+      roundInfo = roundInfo.correctGuess();
+      stdout.writeln('$roundInfo');
+    } else {
+      stdout.writeln('try again');
+      roundInfo = roundInfo.wrongGuess();
     }
-    _roundNum++;
-    firstGuess = false;
+
+    return roundInfo;
   }
 
   void play() {
     var done = false;
-    _numCorrect = 0;
-    _roundNum = 1;
+    var roundInfo = RoundInfo.init();
 
     while (!done) {
-      playRound();
-      if (_roundNum == Config.instance.numRounds) {
-        stdout.writeln('final score $_numCorrect / $_roundNum');
+      roundInfo = playRound(roundInfo);
+      if (roundInfo.isDone(Config.instance.numRounds)) {
+        stdout.writeln('final score $roundInfo');
         done = true;
+      } else {
+        roundInfo = roundInfo.nextRound();
       }
     }
   }
